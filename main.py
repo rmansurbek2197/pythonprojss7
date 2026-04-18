482ebeb4344d4da99b2f83bd1a41d7e3fab53ee2
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 22:36:23 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-import threading
-import time
-
-def vazifa1():
-    for i in range(5):
-        print("Vazifa 1:", i)
-        time.sleep(1)
-
-def vazifa2():
-    for i in range(5):
-        print("Vazifa 2:", i)
-        time.sleep(1)
-
-def vazifa3():
-    for i in range(5):
-        print("Vazifa 3:", i)
-        time.sleep(1)
-
-vazifa1_thread = threading.Thread(target=vazifa1)
-vazifa2_thread = threading.Thread(target=vazifa2)
-vazifa3_thread = threading.Thread(target=vazifa3)
-
-vazifa1_thread.start()
-vazifa2_thread.start()
-vazifa3_thread.start()
-
-vazifa1_thread.join()
-vazifa2_thread.join()
-vazifa3_thread.join()
+N = int(input())
+s = 0
+i = 1
+while i <= N:
+    s += i
+    i += 1
+print(s)
